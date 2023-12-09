4a3bf6fa8fc0d6db3de09f4e26bc2c0374ac5f0e
Lucas Morais <lucas14.morais@gmail.com>
2023-12-09 01:18:51 -0300

    feat(dto): add EventRequestDTO

---
+++ src/main/java/com/youtube/javaspringmicroservices/dtos/EventRequestDTO.java
@@ -0,0 +1,4 @@
+package com.youtube.javaspringmicroservices.dtos;
+
+public record EventRequestDTO(int maxParticipants, int registeredParticipants, String date, String title, String description) {
+}
