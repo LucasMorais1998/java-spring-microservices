f15100fa059a408e19af14c9da81b33c5cd7fe31
Lucas Morais <lucas14.morais@gmail.com>
2023-12-09 01:26:40 -0300

    chore: add @EnableFeignClients

--- src/main/java/com/youtube/javaspringmicroservices/JavaSpringMicroservicesApplication.java
+++ src/main/java/com/youtube/javaspringmicroservices/JavaSpringMicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.youtube.javaspringmicroservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class JavaSpringMicroservicesApplication {
 
 	public static void main(String[] args) {
