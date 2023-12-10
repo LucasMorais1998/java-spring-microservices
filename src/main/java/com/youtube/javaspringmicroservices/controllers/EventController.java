2440d40004f6b20990aca6eeb51a7a966774585a
Lucas Morais <lucas14.morais@gmail.com>
2023-12-10 00:51:28 -0300

    feat(controller): add EventController

---
+++ src/main/java/com/youtube/javaspringmicroservices/controllers/EventController.java
@@ -0,0 +1,38 @@
+package com.youtube.javaspringmicroservices.controllers;
+
+import com.youtube.javaspringmicroservices.domain.Event;
+import com.youtube.javaspringmicroservices.dtos.EventRequestDTO;
+import com.youtube.javaspringmicroservices.dtos.SubscriptionRequestDTO;
+import com.youtube.javaspringmicroservices.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/upcoming")
+    public List<Event> getUpcomingEvents() {
+        return eventService.getUpcomingEvents();
+    }
+
+    @PostMapping
+    public Event createEvent(@RequestBody EventRequestDTO event) {
+        return eventService.createEvent(event);
+    }
+
+    @PostMapping("/{eventId}/register")
+    public void registerParticipant(@PathVariable String eventId, @RequestBody SubscriptionRequestDTO subscriptionRequest) {
+        eventService.registerParticipant(eventId, subscriptionRequest.participantEmail());
+    }
+}
